2576f01554775fa5cefa426fa4f27fb05d3e2a0d
Jason Wharff <fishermanswharff@mac.com>
2016-07-03 15:13:00 -0400

    question 14 solution

--- question-14-inflight-entertainment/lib/cake_question.rb
+++ question-14-inflight-entertainment/lib/cake_question.rb
@@ -1,3 +1,13 @@
-def say_hello
-  return 'hello world'
+require 'pry-byebug'
+# Assume your users will watch exactly two movies
+# Don't make your users watch the same movie twice
+# Optimize for runtime over memory
+def inflight(mins, movie_times)
+  movie_times.each_with_index do |time, i|
+    tmp = movie_times.dup
+    tmp.slice!(i)
+    diff = mins - time
+    return true if tmp.include? diff
+  end
+  return false
 end
--- question-14-inflight-entertainment/spec/cake_question_spec.rb
+++ question-14-inflight-entertainment/spec/cake_question_spec.rb
@@ -1,11 +1,31 @@
 require 'spec_helper'
 require_relative '../lib/cake_question.rb'
 
-RSpec.describe 'Interview Cake Question' do
+RSpec.describe 'Interview Cake Question 14 Inflight Entertainment' do
 
-  describe 'the problem to be solved' do
-    it 'does a thing' do
-      expect(1+1).to eq 2
+  describe 'takes an integer flight_length (in minutes) and an array of integers movie_lengths (in minutes)' do
+    it 'returns a boolean indicating whether there are two numbers in movie_lengths whose sum equals flight_length' do
+      expect(inflight(120, [60,60])).to eq true
+    end
+
+    it 'returns a boolean indicating whether there are two numbers in movie_lengths whose sum equals flight_length' do
+      expect(inflight(124, [60,60])).to eq false
+    end
+
+    it 'returns a boolean indicating whether there are two numbers in movie_lengths whose sum equals flight_length' do
+      expect(inflight(124, [60,64])).to eq true
+    end
+
+    it 'returns a boolean indicating whether there are two numbers in movie_lengths whose sum equals flight_length' do
+      expect(inflight(120, [59,61,90,30])).to eq true
+    end
+
+    it 'returns a boolean indicating whether there are two numbers in movie_lengths whose sum equals flight_length' do
+      expect(inflight(256, [60,60,97,135])).to eq false
+    end
+
+    it 'returns a boolean indicating whether there are two numbers in movie_lengths whose sum equals flight_length' do
+      expect(inflight(120, [60,123,90,70])).to eq false
     end
   end
 end
