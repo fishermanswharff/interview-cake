55152050363e3697ef11bfdfc918994256c8cdbc
Jason Wharff <fishermanswharff@mac.com>
2016-07-05 21:19:35 -0400

    question 19 solution

--- question-19-queue-two-stacks/lib/cake_question.rb
+++ question-19-queue-two-stacks/lib/cake_question.rb
@@ -1,4 +1,44 @@
-require 'byebug'
+require 'pry-byebug'
+
+class QueueFromTwoStacks
+  attr_reader :in_stack, :out_stack, :head, :tail
+
+  def initialize(first_value = nil)
+    node = Node.new(first_value) if first_value
+    @head = node
+    @tail = node
+    @in_stack = Stack.new
+    @out_stack = Stack.new
+
+    in_stack.push(node) if node
+  end
+
+  def enqueue(node)
+    in_stack.push(node)
+    if empty?
+      @head = node
+    end
+    @tail = node
+  end
+
+  def dequeue
+    if in_stack.peek == tail
+      while in_stack.peek != head
+        out_stack.push(in_stack.pop)
+      end
+      @head = out_stack.peek
+      return in_stack.pop
+    elsif out_stack.peek == head
+      node = out_stack.pop
+      @head = node.next_node
+      return node
+    end
+  end
+
+  def empty?
+    @head.nil?
+  end
+end
 
 class Node
   attr_accessor :value, :next_node
@@ -16,12 +56,11 @@ end
 class Stack
   attr_accessor :head
 
-  def initialize(first_value = nil)
-    @head = Node.new(first_value) if first_value
+  def initialize(node = nil)
+    @head = node if node
   end
 
-  def push(value)
-    node = Node.new(value)
+  def push(node)
     node.next_node = @head
     @head = node
   end
--- question-19-queue-two-stacks/spec/cake_question_spec.rb
+++ question-19-queue-two-stacks/spec/cake_question_spec.rb
@@ -1,11 +1,90 @@
 require 'spec_helper'
 require_relative '../lib/cake_question.rb'
 
-RSpec.describe 'Interview Cake Question' do
+RSpec.describe 'Interview Cake Question 19' do
 
-  describe 'the problem to be solved' do
-    it 'does a thing' do
-      expect(1+1).to eq 2
+  let(:queue) { QueueFromTwoStacks.new }
+  let(:stack) { Stack.new }
+
+  describe 'Stack' do
+    before :each do
+      stack.push(Node.new(1))
+      stack.push(Node.new(2))
+      stack.push(Node.new(3))
+      stack.push(Node.new(4))
+      stack.push(Node.new(5))
+      stack.push(Node.new(6))
+    end
+
+    it 'pushes values onto the stack' do
+      expect(stack.peek.value).to eq 6
+    end
+
+    it 'pops the nodes off and returns the node' do
+      
+      node = stack.pop
+      expect(stack.peek.value).to eq 5
+      expect(stack.peek.next_node.value).to eq 4
+      expect(node.value).to eq 6
+      expect(node.next_node.value).to eq 5
+
+      node = stack.pop
+      expect(stack.peek.value).to eq 4
+      expect(stack.peek.next_node.value).to eq 3
+      expect(node.value).to eq 5
+      expect(node.next_node.value).to eq 4
+
+      node = stack.pop
+      expect(stack.peek.value).to eq 3
+      expect(stack.peek.next_node.value).to eq 2
+      expect(node.value).to eq 4
+      expect(node.next_node.value).to eq 3
+    end
+  end
+
+  describe 'Implement a queue with 2 stacks' do
+    describe '#enqueue' do
+      it 'implements enqueue' do
+        queue.enqueue(Node.new(1))
+        expect(queue.in_stack.head.value).to eq 1
+      end
+
+      it 'stacks on the in_stack until a dequeue is called' do
+        queue.enqueue(Node.new(1))
+        queue.enqueue(Node.new(2))
+        expect(queue.in_stack.peek.value).to eq 2
+        expect(queue.head.value).to eq 1
+        expect(queue.tail.value).to eq 2
+      end
+
+      it 'stacks on the in_stack until a dequeue is called' do
+        queue.enqueue(Node.new(1))
+        queue.enqueue(Node.new(2))
+        queue.enqueue(Node.new(3))
+        expect(queue.head.value).to eq 1
+        expect(queue.tail.value).to eq 3
+        expect(queue.out_stack.peek).to be_nil
+      end
+    end
+
+    describe 'dequeue' do
+      before :each do
+        queue.enqueue(Node.new(1))
+        queue.enqueue(Node.new(2))
+        queue.enqueue(Node.new(3))
+        queue.enqueue(Node.new(4))
+        queue.enqueue(Node.new(5))
+      end
+
+      it 'moves the stack over to the out stack when dequeue is called' do
+        node = queue.dequeue
+        expect(queue.out_stack.peek.value).to eq 2
+        expect(node.value).to eq 1
+
+        node = queue.dequeue
+        expect(node.value).to eq 2
+        expect(queue.out_stack.peek.value).to eq 3
+      end
     end
   end
 end
